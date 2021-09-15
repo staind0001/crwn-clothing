9888224d92c596ba53701df9318952e0ebd736bd
Enrique SOTO <57093115+staind0001@users.noreply.github.com>
2021-09-15 10:02:13 +0200

    memoizing user and cart functionalities

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth,getData} from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
+import {createStructuredSelector} from 'reselect'
+import { selectCurrentUser } from './redux/user/user.selector';
 import './App.css';
 
 
@@ -71,8 +73,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
