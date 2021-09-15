ec5b57530501011c623f60107c546944e50a1fe2
Enrique SOTO <57093115+staind0001@users.noreply.github.com>
2021-09-15 09:38:03 +0200

    updating our app with relect, replace cart selectors with memoize selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors' 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import  {Badge}  from 'ui-neumorphism'
 import './cart-icon.styles.scss'
 
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
-        <Badge overlap content={0} bgColor={'#d2691e'}>
+        <Badge overlap content={itemCount} bgColor={'#d2691e'}>
         <ShoppingIcon className= 'shopping-icon'/>
         </Badge>
     </div>
@@ -18,7 +19,11 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = (state) => ({
+itemCount:selectCartItemsCount(state)
+})
+
 export default connect(
-null,
+mapStateToProps,
 mapDispatchToProps
 )(CartIcon) ;
